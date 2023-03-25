7a4fe1171f75d2c01dc606dd0dcfcb8a96dba6e0
Bhavya Madan <121045636+bhavya1006@users.noreply.github.com>
2023-03-25 14:31:39 +0530

    Add files via upload

---
+++ Python/helloWorld.py
@@ -0,0 +1,10 @@
+# Let's begin a small steps to learn Python : 
+# "Hello World" -> This is the first step to display some text,special char 
+#                  or number.
+
+# below here, you may try this syntax 'print("text")'
+print("Hello World!")
+
+# where "" represent the enclosing of the datatype ->STRING (upcoming)
+#       print() is a function or command to outcast data/text on screen
+
---
+++ Python/print().py
@@ -0,0 +1,12 @@
+# Can be also use in many example as shown:
+print()
+
+x = 23
+print(f'Age : x')
+print("Age",x,'can drive.')
+print('1234567890abcdefghijklmnopqrstuvwxyz!@#$%^&*()_+')
+
+print("Some escape sequence: ")
+print("for next line:\\n") # type :"\n" only, "\" is used to represent '\'
+                           #       in the output           
+print("for tabular space:\\t")
