49751d59281f89d1e38c53146cf9ca0ee23979e9
Bhavya Madan <121045636+bhavya1006@users.noreply.github.com>
2023-04-27 23:15:00 +0530

    Add files via upload

---
+++ Python/swappingByThirdVariable.py
@@ -0,0 +1,13 @@
+# swapping with temp variable
+
+var1 = 3
+var2 = 4
+print("Before Swapping values -\n")
+print("Value 1:",var1,"\nValue 2:",var2)
+
+temp = var1
+var1 = var2
+var2 = temp
+
+print("\nAfter Swapping values -\n")
+print("Value 1:",var1,"\nValue 2:",var2)
