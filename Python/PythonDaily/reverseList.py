25d990304761ed79a7047bcc37b80a01cd31a67e
Bhavya Madan <121045636+bhavya1006@users.noreply.github.com>
2023-05-20 14:14:12 +0530

    Add files via upload

---
+++ Python/PythonDaily/reverseList.py
@@ -0,0 +1,3 @@
+def revList(n):
+    return [x for x in range(n,0,-1)]
+print(revList(5))
---
+++ Python/PythonDaily/smallInt.py
@@ -0,0 +1,5 @@
+def smallInt(n):
+    return sorted(n)[0]
+    
+li = [4,201,-7,3,45,90,2]
+print(smallInt(li))
