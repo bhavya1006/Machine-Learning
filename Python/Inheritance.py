2ba747f629adeaa85ce25411643b6a00d0b8d87a
Bhavya Madan <121045636+bhavya1006@users.noreply.github.com>
2023-06-01 01:19:34 +0530

    Update Inheritance.py

--- Python/Inheritance.py
+++ Python/Inheritance.py
@@ -1,26 +1,38 @@
 class Person:
-    def __init__(s,n,age):
-        s.name=n
-        s.age=age
-    def display(s):
-        print(s.name,s.age,end=' ')
-class Emp(Person):
-    def __init__(s,salary,n,a):
-        Person.__init__(s,n,a)
-        s.salary = salary
-    def display(s):
-        super().display()
-        print(s.salary)
-class admin(Emp):
-    def __init__(s,pre,n,a,sal):
-        s.pre=pre
-        Emp.__init__(s,sal,n,a)
-    def display(s):
-        print(s.pre)
-        super().display()
-        
-a = admin("5g%##",34000,"guru",34)
-ab = admin("6/^&%",45002,"pita",51)
 
-a.display()
-ab.display()
+    def __init__(s,name,age,gender,address):
+        s.name = name
+        s.age = age
+        s.gender = gender
+        s.address = address
+
+    def display(s):
+        print(f'Name: {s.name} Age: {s.age} Gender: {s.gender} Address: {s.address}')
+
+class Employee(Person):
+
+    __empId = 'C000{}'
+
+    def __init__(s,empId,salary,designation,n,a,g,add):
+        s.empId = Employee.__empId.format(empId)
+        s.salary = salary
+        s.desg = designation
+        Person.__init__(s,n,a,g,add)
+
+    def display(s):
+        print(f"Employee Id: {s.empId} Salary: {s.salary} Designation: {s.desg}")
+        super().display()
+
+class admin(Employee):
+
+    def __init__(s,access,e,sal,d,n,a,g,add):
+        s.access = access
+        Employee.__init__(s,e,sal,d,n,a,g,add)
+    
+    def display(s):
+        print(f"Admin Permit: {s.access}")
+        super().display()
+
+
+master = admin("Yes",25,34900,"Sen. Developer","Arya Kohli",28,'F',"Gwalior")
+master.display()
